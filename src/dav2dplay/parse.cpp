0aee3260faf5384a90c3583c90572b27738a2c72
Elijah Cirioli <50853223+ElijahCirioli@users.noreply.github.com>
2022-11-02 22:52:53 -0700

    encapsulated webm parsing

---
+++ src/dav2dplay/parse.cpp
@@ -0,0 +1,165 @@
+#include "parse.h"
+
+using namespace webm;
+
+class Av1Callback : public Callback {
+   public:
+    void
+    init(ParseContext *context)
+    {
+        this->av1_track_number = -1;
+        this->context = context;
+    }
+
+    bool
+    found_av1_track()
+    {
+        return this->av1_track_number != -1;
+    }
+
+    Status
+    OnTrackEntry(const ElementMetadata &metadata, const TrackEntry &track_entry) override
+    {
+        // this is assuming at most one video track
+        if (track_entry.codec_id.is_present() &&
+            track_entry.codec_id.value() == "V_AV1") {
+            this->av1_track_number = track_entry.track_number.value();
+        }
+        return Status(Status::kOkCompleted);
+    }
+
+    Status
+    OnSimpleBlockBegin(const ElementMetadata &metadata, const SimpleBlock &simple_block,
+                       Action *action) override
+    {
+        if (simple_block.track_number == this->av1_track_number) {
+            *action = Action::kRead;
+        }
+        else {
+            *action = Action::kSkip;
+        }
+        return Status(Status::kOkCompleted);
+    }
+
+    Status
+    OnBlockBegin(const ElementMetadata &metadata, const Block &block,
+                 Action *action) override
+    {
+        if (block.track_number == this->av1_track_number) {
+            *action = Action::kRead;
+        }
+        else {
+            *action = Action::kSkip;
+        }
+        return Status(Status::kOkCompleted);
+    }
+
+    Status
+    OnFrame(const FrameMetadata &, Reader *reader,
+            std::uint64_t *bytes_remaining) override
+    {
+        //  sanity checks
+        assert(reader != nullptr);
+        assert(bytes_remaining != nullptr);
+        if (*bytes_remaining == 0) {
+            return Status(Status::kOkCompleted);
+        }
+
+        // resize the buffer if it's not big enough
+        context->size = *bytes_remaining;
+        while (context->size > context->capacity) {
+            delete[] context->data;
+            context->capacity *= 2;
+            context->data = new std::uint8_t[context->capacity];
+        }
+
+        // read frame data until there's no more to read
+        std::uint8_t *buffer_location = context->data;
+        std::uint64_t num_read;
+        Status status;
+        do {
+            status = reader->Read(*bytes_remaining, buffer_location, &num_read);
+            buffer_location += num_read;
+            *bytes_remaining -= num_read;
+        } while (status.code == Status::kOkPartial);
+
+        // stop parsing for now
+        return Status(Status::kWouldBlock);
+    }
+
+   private:
+    std::uint64_t av1_track_number;
+    ParseContext *context;
+};
+
+typedef struct ParseInternalState {
+    FileReader *reader;
+    WebmParser *parser;
+    Av1Callback *callback;
+    FILE *file;
+} ParseInternalState;
+
+void
+parse_init(ParseContext **context, char *file_name)
+{
+    // create the internal state
+    ParseInternalState *state = new ParseInternalState;
+
+    // open the input file
+    state->file = std::fopen(file_name, "rb");
+
+    // create the webmparser objects
+    state->callback = new Av1Callback();
+    state->reader = new FileReader(state->file);
+    state->parser = new WebmParser();
+
+    // allocate the ParseContext
+    ParseContext *parse_context = new ParseContext;
+    *context = parse_context;
+
+    // populate the ParseContext
+    parse_context->internal_state = (void *)state;
+    parse_context->size = 0;
+    parse_context->timecode = 0;
+
+    // create the data buffer
+    parse_context->capacity = 512;
+    parse_context->data = new std::uint8_t[parse_context->capacity];
+
+    // initialize the callback class
+    state->callback->init(parse_context);
+}
+
+void
+parse_destroy(ParseContext *context)
+{
+    // clean up internal state
+    ParseInternalState *state = (ParseInternalState *)context->internal_state;
+    fclose(state->file);
+    delete state->callback;
+    delete state->reader;
+    delete state->parser;
+    delete state;
+
+    // clean up context
+    delete[] context->data;
+    delete context;
+}
+
+int
+parse_get_next_frame(ParseContext *context)
+{
+    ParseInternalState *state = (ParseInternalState *)context->internal_state;
+
+    Status status = state->parser->Feed(state->callback, state->reader);
+    if (status.completed_ok()) {
+        if (state->callback->found_av1_track()) {
+            return PARSE_END_OF_FILE;
+        }
+        return PARSE_NO_AV1_TRACK;
+    }
+    else if (status.code == Status::kWouldBlock) {
+        return PARSE_OK;
+    }
+    return PARSE_ERROR;
+}
--- src/dav2dplay/parse.h
+++ src/dav2dplay/parse.h
@@ -1,18 +1,34 @@
+#ifndef PARSE_H
+#define PARSE_H
 
+#include <cassert>
+#include <string.h>
+
+#include <webm/callback.h>
+#include <webm/file_reader.h>
+#include <webm/status.h>
+#include <webm/webm_parser.h>
+
+#define PARSE_OK 0b00000000
+#define PARSE_END_OF_FILE 0b00000001
+#define PARSE_ERROR 0b00000010
+#define PARSE_NO_AV1_TRACK 0b00000100
 
 typedef struct ParseContext {
     std::uint8_t *data;
     std::size_t size;
     std::size_t capacity;
-    std::uint64_t timecode;
-    webm::FileParser;
-};
+    std::uint64_t timecode;  // the timestamp of the frame in milliseconds
+    void *internal_state;
+} ParseContext;
 
 void
-parse_init(struct ParseContext *context, char *file_name);
+parse_init(ParseContext *context, char *file_name);
 
 void
-parse_destroy(struct ParseContext *context);
+parse_destroy(ParseContext *context);
 
 int
-parse_get_next_frame(struct ParseContext *context);
+parse_get_next_frame(ParseContext *context);
+
+#endif
