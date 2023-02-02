878d4d8910abf1d026d89f3ba75ed3cb9211e617
Starbuck5 <46412508+Starbuck5@users.noreply.github.com>
2023-02-01 22:16:47 -0800

    Fix for new sav1_set_error signature

--- src/sav1.c
+++ src/sav1.c
@@ -1,34 +1,31 @@
 #include "sav1.h"
 #include "sav1_internal.h"
 
-#define CHECK_CTX_VALID(ctx, context)                                              \
-    if (ctx == NULL) {                                                             \
-        sav1_set_error(context,                                                    \
-                       "Uninitialized context: sav1_create_context() not called"); \
-        return -1;                                                                 \
+#define CHECK_CTX_VALID(ctx)                                                            \
+    if (ctx == NULL) {                                                                  \
+        sav1_set_error(ctx, "Uninitialized context: sav1_create_context() not called"); \
+        return -1;                                                                      \
     }
 
-#define CHECK_CTX_INITIALIZED(ctx, context)                                        \
-    if (!ctx->is_initialized) {                                                    \
-        sav1_set_error(context,                                                    \
-                       "Uninitialized context: sav1_create_context() not called"); \
-        return -1;                                                                 \
+#define CHECK_CTX_INITIALIZED(ctx)                                                      \
+    if (!ctx->is_initialized) {                                                         \
+        sav1_set_error(ctx, "Uninitialized context: sav1_create_context() not called"); \
+        return -1;                                                                      \
     }
 
-#define CHECK_CTX_CRITICAL_ERROR(ctx, context) \
-    if (ctx->critical_error_flag) {            \
-        return -1;                             \
+#define CHECK_CTX_CRITICAL_ERROR(ctx) \
+    if (ctx->critical_error_flag) {   \
+        return -1;                    \
     }
 
 int
 sav1_get_video_frame(Sav1Context *context, Sav1VideoFrame **frame)
 {
     Sav1InternalContext *ctx = (Sav1InternalContext *)context->internal_state;
-    CHECK_CTX_VALID(ctx, context)
-    CHECK_CTX_INITIALIZED(ctx, context)
-    CHECK_CTX_CRITICAL_ERROR(ctx, context)
+    CHECK_CTX_VALID(ctx)
+    CHECK_CTX_INITIALIZED(ctx)
+    CHECK_CTX_CRITICAL_ERROR(ctx)
 }
-
 
 int
 sav1_start_playback(Sav1Context *context)
