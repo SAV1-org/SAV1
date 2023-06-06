ad911d26e66904a7b390da4768359f9eaadf01b2
ElijahCirioli <50853223+ElijahCirioli@users.noreply.github.com>
2023-06-06 21:17:49 +0000

    Deploying to gh-pages from @ SAV1-org/SAV1@01a294d78cd362c3d40eedf2f81157d5fc3a6518 🚀

--- search/variables_1.js
+++ search/variables_1.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['data_0',['data',['../struct_sav1_audio_frame.html#a42d842b27bbaa949df22c93db7250f05',1,'Sav1AudioFrame::data()'],['../struct_sav1_video_frame.html#af0fde7ef63297d5c59c7a3ac889f0f30',1,'Sav1VideoFrame::data()']]],
+  ['data_0',['data',['../struct_sav1_audio_frame.html#a0fa704f7d7aa5484fa59ab81a7ea2ff0',1,'Sav1AudioFrame::data()'],['../struct_sav1_video_frame.html#af0fde7ef63297d5c59c7a3ac889f0f30',1,'Sav1VideoFrame::data()']]],
   ['desired_5fpixel_5fformat_1',['desired_pixel_format',['../struct_sav1_settings.html#a0fad556ea8343a2d46174fdb3fc31d01',1,'Sav1Settings']]],
   ['duration_2',['duration',['../struct_sav1_audio_frame.html#a5cc17f69134581c2f97aa790c7ffdbb3',1,'Sav1AudioFrame']]]
 ];
--- search/variables_8.js
+++ search/variables_8.js
@@ -1,7 +1,8 @@
 var searchData=
 [
-  ['sav1_5fhas_5fownership_0',['sav1_has_ownership',['../struct_sav1_audio_frame.html#aa47ed519af14374ed6796619c4c762d6',1,'Sav1AudioFrame::sav1_has_ownership()'],['../struct_sav1_video_frame.html#ad021e088a6a37a942eef66be80b5a091',1,'Sav1VideoFrame::sav1_has_ownership()']]],
-  ['sentinel_1',['sentinel',['../struct_sav1_audio_frame.html#a6f7d6a14a8ae4f11c7d492347611a52f',1,'Sav1AudioFrame::sentinel()'],['../struct_sav1_video_frame.html#ac2a7693b21c7692a1a66adc6d5bc4729',1,'Sav1VideoFrame::sentinel()']]],
-  ['size_2',['size',['../struct_sav1_audio_frame.html#a919b489688a65eef74e28cbe8e0dbe04',1,'Sav1AudioFrame::size()'],['../struct_sav1_video_frame.html#a81e4d4367e74dc6dcfc5003530902ee9',1,'Sav1VideoFrame::size()']]],
-  ['stride_3',['stride',['../struct_sav1_video_frame.html#a7ce11a53617571128b35e47068ca15a9',1,'Sav1VideoFrame']]]
+  ['sample_5fsize_0',['sample_size',['../struct_sav1_audio_frame.html#a142b9ce6adec8585fb654a0d3427b9fa',1,'Sav1AudioFrame']]],
+  ['sav1_5fhas_5fownership_1',['sav1_has_ownership',['../struct_sav1_audio_frame.html#aa47ed519af14374ed6796619c4c762d6',1,'Sav1AudioFrame::sav1_has_ownership()'],['../struct_sav1_video_frame.html#ad021e088a6a37a942eef66be80b5a091',1,'Sav1VideoFrame::sav1_has_ownership()']]],
+  ['sentinel_2',['sentinel',['../struct_sav1_audio_frame.html#a6f7d6a14a8ae4f11c7d492347611a52f',1,'Sav1AudioFrame::sentinel()'],['../struct_sav1_video_frame.html#ac2a7693b21c7692a1a66adc6d5bc4729',1,'Sav1VideoFrame::sentinel()']]],
+  ['size_3',['size',['../struct_sav1_audio_frame.html#a919b489688a65eef74e28cbe8e0dbe04',1,'Sav1AudioFrame::size()'],['../struct_sav1_video_frame.html#a81e4d4367e74dc6dcfc5003530902ee9',1,'Sav1VideoFrame::size()']]],
+  ['stride_4',['stride',['../struct_sav1_video_frame.html#a7ce11a53617571128b35e47068ca15a9',1,'Sav1VideoFrame']]]
 ];
